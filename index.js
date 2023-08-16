c62fa7044ff5d131b419001d59472d2f51c7d707
bharatgupta695 <bharatgupta68823@gmail.com>
2023-08-16 23:25:28 +0530

    product-ready

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const socketIO = require("socket.io");
 
 const app=express();
-const port= 4000|| process.env.PORT ;
+const port=  process.env.PORT ;
 
 
 const users=[{}];
